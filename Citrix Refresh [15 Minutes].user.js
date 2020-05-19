8482610ce5032405dc6e13a0ea833bd827ad7338
lukasubo <lukasubo@gmail.com>
2020-05-19 17:57:24 -0400

    Update Citrix Refresh [15 Minutes].user.js

--- Citrix Refresh [15 Minutes].user.js
+++ Citrix Refresh [15 Minutes].user.js
@@ -1,6 +1,9 @@
 // ==UserScript==
-// @name         Citrix Refresh [15 Minutes]
+// @name         Citrix Refresh
 // @include      https://bolconnect.husky.ca/Citrix/StoreWeb/
+// @version      1.0
+// @description  Refreshes every 15 minutes.
+// @author       lukasubo
 // ==/UserScript==
 
 setTimeout(function(){ location.reload(); }, 15*60*1000);
