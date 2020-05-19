bafa8471d92654cfcd8d81884e9536a0088a6f09
lukasubo <lukasubo@gmail.com>
2020-05-19 17:53:35 -0400

    Add files via upload

---
+++ Citrix Refresh [15 Minutes].user.js
@@ -0,0 +1,6 @@
+// ==UserScript==
+// @name         Citrix Refresh [15 Minutes]
+// @include      https://bolconnect.husky.ca/Citrix/StoreWeb/
+// ==/UserScript==
+
+setTimeout(function(){ location.reload(); }, 15*60*1000);
