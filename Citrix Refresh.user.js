6dd83bb5b00fdcace58d81790a1785f529c43b48
lukasubo <lukasubo@gmail.com>
2020-07-02 09:51:05 -0400

    Added auto-update URL

--- Citrix Refresh.user.js
+++ Citrix Refresh.user.js
@@ -2,9 +2,10 @@
 // @name         Citrix Refresh
 // @include      https://bolconnect.husky.ca/Citrix/StoreWeb/
 // @include      https://luxconnect.husky.ca/Citrix/StoreWeb/
-// @version      1.1
+// @version      1.2
 // @description  Refreshes every 15 minutes.
 // @author       lukasubo
+// @updateURL    https://github.com/lukasubo/autorefreshcitrix/raw/master/Citrix%20Refresh.user.js
 // ==/UserScript==
 
 setTimeout(function(){ location.reload(); }, 15*60*1000);
